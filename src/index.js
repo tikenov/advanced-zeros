ff1129a8c30905c4188a6287d1a477ddcfb0a809
tikenov <azamatkz90@gmail.com>
2019-03-14 17:00:27 +0600

    solutipn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+function primeFactors(number) {
+  var map = [];
+  for (let i = 2; i <= number; i++)
+    while (number % i === 0) {
+      if (map[i]) map[i]++;
+      else map[i] = 1;
+      number /= i;
+    }
+  return map;
 }
+
+module.exports = function getZerosCount(number, base) {
+  var ans = Number.MAX_SAFE_INTEGER;
+  var p = primeFactors(base);
+  var k = Object.keys(p);
+  for (let i = 0; i < k.length; i++) {
+    var prime = k[i];
+    var count = p[k[i]];
+    var temp = 0;
+    while (prime < number) {
+      temp += Math.floor(number / prime);
+      prime *= k[i];
+    }
+    temp = Math.floor(temp / count);
+    ans = temp <= ans ? temp : ans;
+  }
+  return ans;
+};
